a7dbbba0e7fd82b69aed3d6955f8d1e78ea81129
Kermit <kermitwk@gmail.com>
2017-02-07 23:26:14 +0800

    reset indentation

--- src/js/datasource.js
+++ src/js/datasource.js
@@ -3,15 +3,15 @@ const WIKI_SEARCH_API = 'https://en.wikipedia.org/w/api.php?action=query&prop=ex
 function DataProvider () {}
 
 DataProvider.getData = function(searchVal, callback) {
-    window.wikiCallback = callback;
-    jsonp(WIKI_SEARCH_API + searchVal);
+  window.wikiCallback = callback;
+  jsonp(WIKI_SEARCH_API + searchVal);
 };
 
 function jsonp (url) {
-    var script = document.createElement('script');
-    script.setAttribute('src', url + '&callback=wikiCallback');
-    document.head.appendChild(script);
-    document.head.removeChild(script);
+  var script = document.createElement('script');
+  script.setAttribute('src', url + '&callback=wikiCallback');
+  document.head.appendChild(script);
+  document.head.removeChild(script);
 }
 
 export default DataProvider;
--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,50 @@
 var path = require('path');
 
 var config = {
-    entry: {
-        main: path.join(__dirname, 'src/js', 'main.js')
-    },
+  entry: {
+    main: path.join(__dirname, 'src/js', 'main.js')
+  },
 
-    output: {
-        path: path.join(__dirname, 'build'),
-        filename: 'bundle.js',
-        publicPath: '/assets/'
-    },
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/assets/'
+  },
 
-    devServer: {
-        contentBase: 'build/',
-        inline: true,
-        port: 8080
-    },
+  devServer: {
+    contentBase: 'build/',
+    inline: true,
+    port: 8080
+  },
 
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: path.join(__dirname, 'node_modules'),
-                loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: path.join(__dirname, 'node_modules'),
+        loader: 'babel-loader',
 
-                options: {
-                    presets: [
-                        'es2015',
-                        'react'
-                    ]
-                }
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            sourceMap: true
-                        }
-                    }
-                ]
+        options: {
+          presets: [
+            'es2015',
+            'react'
+          ]
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
             }
+          }
         ]
-    }
+      }
+    ]
+  }
 };
 
 module.exports = config;
